66f0c345ae340a506eb10d2f260e677cd54199d7
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2023-03-30 21:06:32 +0100

    Done with the contact page

---
+++ src/components/choose.jsx
@@ -0,0 +1,10 @@
+
+
+
+const Choose = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/home.jsx
+++ src/home.jsx
@@ -12,7 +12,7 @@ import Footer from "./components/footer"
 const Home = () => {
 
     return(
-        <div>
+        <div className="cover">
             <Header />
             <Hero />
             <Services />
